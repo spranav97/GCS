392f4e51a5354ecb212c59bc54b889fed78c84d5
pranav <42641422+spranav97@users.noreply.github.com>
2019-10-04 09:43:15 +0530

    Add files via upload

---
+++ joystick test.py
@@ -0,0 +1,24 @@
+import pygame
+
+pygame.init()
+j = pygame.joystick.Joystick(0)
+j.init()
+
+try:
+    while True:
+        events = pygame.event.get()
+        for event in events:
+            if event.type == pygame.JOYAXISMOTION:
+                print(event.dict, event.axis)
+            elif event.type == pygame.JOYBALLMOTION:
+                print(event.dict, event.ball)
+            elif event.type == pygame.JOYBUTTONDOWN:
+                print(event.dict, event.button, 'pressed')
+            elif event.type == pygame.JOYBUTTONUP:
+                print(event.dict, event.button, 'released')
+            elif event.type == pygame.JOYHATMOTION:
+                print(event.dict, event.hat)
+
+except KeyboardInterrupt:
+    print("EXITING NOW")
+    j.quit()
