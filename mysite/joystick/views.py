b1282435c369ded48112640ea78949faea1a48a1
spranav97 <spranav97@hotmail.com>
2019-10-07 07:32:25 +0530

    support for jostick using java script

---
+++ mysite/joystick/views.py
@@ -0,0 +1,3 @@
+from django.shortcuts import render
+
+def room(request):return render(request, 'joystick/room.html')
---
+++ mysite/mysite/routing.py
@@ -0,0 +1,17 @@
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing,joystick.routing
+
+application = ProtocolTypeRouter({
+    # (http->django views is added by default)
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            joystick.routing.websocket_urlpatterns
+        )
+    ),
+})
