c00dde2a4c3d5a13b59ad3bc884ae60ce96de8a8
spranav97 <spranav97@hotmail.com>
2019-10-09 17:39:59 +0530

    all joystick data is transmitted in calculatable format

--- mysite/mysite/routing.py
+++ mysite/mysite/routing.py
@@ -3,15 +3,11 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing,joystick.routing
 
 application = ProtocolTypeRouter({
-    # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
         URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    ),
-    'websocket2': AuthMiddlewareStack(
-        URLRouter(
+            #chat.routing.websocket_urlpatterns
             joystick.routing.websocket_urlpatterns
         )
-    ),
+
+    )
 })
